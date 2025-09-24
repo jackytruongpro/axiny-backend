0be6251d952cbe9050c59bf9368afefd0bc2d37e
jackytruongpro <jacky-t@hotmail.fr>
2025-09-24 12:01:34 +0200

    Fix Plumber annotations and endpoint definition

--- server.R
+++ server.R
@@ -2,27 +2,29 @@
 
 library(plumber)
 
-# Récupérer le port fourni par Cloud Run (par défaut 8080 si non défini)
-port <- as.integer(Sys.getenv("PORT", "8080"))
-
 #* @apiTitle Axiny Snapshot Service
 
 #* Receive an app.R file and return snapshots ZIP
 #* @param file:file The Shiny app file
 #* @serializer contentType list(type="application/zip")
-function(file) {
+#* @post /snapshot
+function(req, res){
   tmpdir <- tempfile("axiny")
   dir.create(tmpdir)
   app_path <- file.path(tmpdir, "app.R")
-  writeBin(file$datapath, app_path)
+  writeBin(req$postBody, app_path)  # plus fiable que file$datapath dans Plumber
   file.copy("/srv/shiny-server/snapshot.R", tmpdir)
   oldwd <- setwd(tmpdir)
   system("Rscript snapshot.R", intern = TRUE)
   setwd(oldwd)
   zipfile <- file.path(tmpdir, "snapshots.zip")
   utils::zip(zipfile, list.files(file.path(tmpdir, "snapshots"), full.names = TRUE))
-  readBin(zipfile, what="raw", n=file.info(zipfile)$size)
-} -> pr_endpoint
+  bin <- readBin(zipfile, what="raw", n=file.info(zipfile)$size)
+  res$setHeader("Content-Type", "application/zip")
+  res$body <- bin
+  res
+} -> snapshot_endpoint
 
-pr <- plumber::plumb("/srv/shiny-server/server.R")
-pr$run(host = "0.0.0.0", port = port)
+# Plumb & run
+pr <- plumber::plumb(file = "server.R")
+pr$run(host = "0.0.0.0", port = as.integer(Sys.getenv("PORT", "8080")))
