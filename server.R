f7327e03707856883f18a2179788cc972a29f6aa
jackytruongpro <jacky-t@hotmail.fr>
2025-09-24 04:52:20 +0200

    fix port

--- server.R
+++ server.R
@@ -2,31 +2,27 @@
 
 library(plumber)
 
+# Récupérer le port fourni par Cloud Run (par défaut 8080 si non défini)
+port <- as.integer(Sys.getenv("PORT", "8080"))
+
 #* @apiTitle Axiny Snapshot Service
 
 #* Receive an app.R file and return snapshots ZIP
 #* @param file:file The Shiny app file
 #* @serializer contentType list(type="application/zip")
 function(file) {
-  # Create temp dir
   tmpdir <- tempfile("axiny")
   dir.create(tmpdir)
   app_path <- file.path(tmpdir, "app.R")
   writeBin(file$datapath, app_path)
-
-  # Copy snapshot.R into tmpdir
   file.copy("/srv/shiny-server/snapshot.R", tmpdir)
-
-  # Run snapshot script
   oldwd <- setwd(tmpdir)
   system("Rscript snapshot.R", intern = TRUE)
   setwd(oldwd)
-
-  # Create zip of snapshots folder
   zipfile <- file.path(tmpdir, "snapshots.zip")
   utils::zip(zipfile, list.files(file.path(tmpdir, "snapshots"), full.names = TRUE))
+  readBin(zipfile, what="raw", n=file.info(zipfile)$size)
+} -> pr_endpoint
 
-  # Read and return zip
-  zip_data <- readBin(zipfile, what="raw", n=file.info(zipfile)$size)
-  zip_data
-}
+pr <- plumber::plumb("/srv/shiny-server/server.R")
+pr$run(host = "0.0.0.0", port = port)
