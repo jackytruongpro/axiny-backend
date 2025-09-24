3da661c2009e2419911f9d14597f0bc0245408ac
jackytruongpro <jacky-t@hotmail.fr>
2025-09-24 14:09:19 +0200

    Resolve server and port issue

--- server.R
+++ server.R
@@ -8,22 +8,28 @@ library(plumber)
 #* @param file:file The Shiny app file
 #* @serializer contentType list(type="application/zip")
 #* @post /snapshot
-function(req, res){
-  tmpdir <- tempfile("axiny")
+function(req, res) {
+  tmpdir   <- tempfile("axiny")
   dir.create(tmpdir)
   app_path <- file.path(tmpdir, "app.R")
-  writeBin(req$postBody, app_path)  # plus fiable que file$datapath dans Plumber
+  writeBin(req$postBody, app_path)
   file.copy("/srv/shiny-server/snapshot.R", tmpdir)
   oldwd <- setwd(tmpdir)
   system("Rscript snapshot.R", intern = TRUE)
   setwd(oldwd)
   zipfile <- file.path(tmpdir, "snapshots.zip")
   utils::zip(zipfile, list.files(file.path(tmpdir, "snapshots"), full.names = TRUE))
-  bin <- readBin(zipfile, what="raw", n=file.info(zipfile)$size)
+  bin <- readBin(zipfile, what = "raw", n = file.info(zipfile)$size)
   res$setHeader("Content-Type", "application/zip")
   res$body <- bin
   res
-} -> snapshot_endpoint
+}
 
-pr <- plumber::plumb(file = "server.R")
-pr$run(host = "0.0.0.0", port = as.integer(Sys.getenv("PORT", "8080")))
+# Charger les endpoints annotés déjà dans l’environnement
+pr <- plumber::pr()
+
+# Démarrer le serveur sur le port Cloud Run
+pr$run(
+  host = "0.0.0.0",
+  port = as.integer(Sys.getenv("PORT", "8080"))
+)
